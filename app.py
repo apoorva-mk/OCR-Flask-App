611cc02e116cda1dcd364f6c238deca57dd01ba2
apoorva-mk <apoorva.mk99@gmail.com>
2020-03-13 00:59:52 +0530

    dynamic port

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
-from flask import Flask
-from flask import request
-import ocr
+# from flask import Flask
+# from flask import request
+# import ocr
+# import os
+# port = int(os.environ.get("PORT", 5000))
+# app = Flask(__name__)
+
+
+# @app.route('/', methods=["GET","POST"])
+# def hello_world():
+#     if request.method == 'POST':
+#         req_data = request.get_json()
+#         encoded_string = req_data["base64encodedimage"]
+#         text = ocr.obtain_text_from_image(encoded_string.encode())
+#         return text
+#     else:
+#         return "This is an simple OCR API, made by Alphageek"
+
+# if __name__ == '__main__':
+#     app.run(port=port)
+
+
 import os
-port = int(os.environ.get("PORT", 5000))
+from flask import Flask
+
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello world!"
 
-@app.route('/', methods=["GET","POST"])
-def hello_world():
-    if request.method == 'POST':
-        req_data = request.get_json()
-        encoded_string = req_data["base64encodedimage"]
-        text = ocr.obtain_text_from_image(encoded_string.encode())
-        return text
-    else:
-        return "This is an simple OCR API, made by Alphageek"
-
-if __name__ == '__main__':
-    app.run(port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
