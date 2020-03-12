51deca3b49e9ca0353f805ad83c0a31611f9b74b
apoorva-mk <apoorva.mk99@gmail.com>
2020-03-13 01:05:13 +0530

    dynamic port

--- app.py
+++ app.py
@@ -21,13 +21,23 @@
 
 
 import os
+import ocr
+from flask import request
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+@app.route("/", methods=["GET","POST"])
+def home():
+	if request.method == 'GET'
+    	return "This is an simple OCR API, made by Alphageek"
+
+    else:
+		req_data = request.get_json()
+		encoded_string = req_data["base64encodedimage"]
+		text = ocr.obtain_text_from_image(encoded_string.encode())
+		return text
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
