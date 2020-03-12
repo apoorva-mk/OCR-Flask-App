91496390d7a91030e1dd2fdb631fe97864a8bcf4
apoorva-mk <apoorva.mk99@gmail.com>
2020-03-12 23:01:42 +0530

    flask app added

---
+++ ocr_app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import request
+import ocr
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET","POST"])
+def hello_world():
+    if request.method == 'POST':
+        req_data = request.get_json()
+        encoded_string = req_data["base64encodedimage"]
+        text = ocr.obtain_text_from_image(encoded_string.encode())
+        return text
+    else:
+        return "This is an simple OCR API, made by Alphageek"
+
+if __name__ == '__main__':
+    app.run(port='4001')
